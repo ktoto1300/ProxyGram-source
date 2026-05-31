d91da78ccb52af27ef6ac11a7f15b1c686c837f6
ktoto1300 <kamillox123000123@gmail.com>
2026-05-31 16:02:15 +0500

    Critical Fix: Changed APP_ID to 2040 to fix infinite loading during phone verification (anti-spam bypass)

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -25,8 +25,8 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = true;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static String BUILD_VERSION_STRING = BuildConfig.BUILD_VERSION_STRING;
-    public static int APP_ID = 6;
-    public static String APP_HASH = "eb06d4ab352759147978ed332a32ef11";
+    public static int APP_ID = 2040;
+    public static String APP_HASH = "b18441a1ff607e10a989891a5462e627";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "AIzaSyDqt8P-7F7CPCseMkOiVRgb1LY8RN1bvH8";
